a9278b00a8beaa64a2f1b12f3c78d4943ed7467f
Magalí Menchón <magalimenchon@gmail.com>
2021-09-04 12:56:37 -0300

    Se agregan ejercicios de patrones de la clase

---
+++ Ejemplo_Clase1_FactoryMethod/src/patron/factorymethod/clases/room/OrdinaryRoom.java
@@ -0,0 +1,9 @@
+package patron.factorymethod.clases.room;
+
+import patron.factorymethod.abstracta.Room;
+
+
+public class OrdinaryRoom extends Room{
+	@Override
+	public void connect(Room room) {}
+}
